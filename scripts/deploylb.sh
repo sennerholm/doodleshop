a0f701a9800641c02a8342e1fe62214a5253a55f
Mikael Sennerholm <mikael@sennerholm.net>
2015-01-20 20:23:09 +0100

    Second edition of haproxy

--- scripts/deploylb.sh
+++ scripts/deploylb.sh
@@ -3,6 +3,7 @@ set -ex
 INSTANCE=${DOODLE_ENV}
 VERSION=${GO_PIPELINE_LABEL}
 PORT_PREFIX=$1
-sudo docker rm -f haproxy-${INSTANCE}
-sudo docker run -d --name haproxy-${INSTANCE} -l doodleshop-${INSTANCE}-green:green -l doodleshop-${INSTANCE}-blue:blue -p ${PORT_PREFIX}8080:80 dockerfile/haproxy
+GW=`route | grep default | awk '{print $2}'`
+sudo docker rm -f haproxy-${INSTANCE} || /bin/true
+sudo docker run -d --name haproxy-${INSTANCE} --add-host gw:${GW}-p ${PORT_PREFIX}8080:80 dockerfile/haproxy
 
