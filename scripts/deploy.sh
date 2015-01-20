d7e5065bd49cb3a66b75e31e116cfdcea49dba36
Mikael Sennerholm <mikael@sennerholm.net>
2015-01-20 19:06:31 +0100

    First try of deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,7 @@
 INSTANCE=${DOODLE_ENV}
 VERSION=${GO_PIPELINE_LABEL}
 PORT_PREFIX=$1
-echo running sudo docker rm -f doodleshop-${INSTANCE}
+set -ex 
 sudo docker rm -f doodleshop-${INSTANCE}
-echo running sudo docker run -d --name doodleshop-${INSTANCE} -p ${PORT_PREFIX}8080:8080 doodleshop:$VERSION
-sudo docker run -d --name doodleshop-${INSTANCE} -p ${PORT_PREFIX}8080:8080 doodleshop:$VERSION
+sudo docker run -d --name doodleshop-${INSTANCE} doodleshop:$VERSION
 
