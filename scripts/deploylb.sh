1ca03a2beec86a02b728408208e779433c3aca43
Mikael Sennerholm <mikael@sennerholm.net>
2015-01-20 22:35:09 +0100

    haproxy.cfg now in a directory from the go pipeline shared to the boot2docker vm

--- scripts/deploylb.sh
+++ scripts/deploylb.sh
@@ -5,5 +5,5 @@ VERSION=${GO_PIPELINE_LABEL}
 PORT_PREFIX=$1
 GW=`route | grep default | awk '{print $2}'`
 sudo docker rm -f haproxy-${INSTANCE} || /bin/true
-sudo docker run -d -v haproxy:/haproxy-override --name haproxy-${INSTANCE} --add-host gw:${GW} -p ${PORT_PREFIX}8080:80 dockerfile/haproxy
+sudo docker run -d -v /vagrant/haproxy:/haproxy-override --name haproxy-${INSTANCE} --add-host gw:${GW} -p ${PORT_PREFIX}8080:80 dockerfile/haproxy
 
