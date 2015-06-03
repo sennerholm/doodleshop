ae5276cf002cbeec0144aa227e581a0a92c46f83
Mikael Sennerholm <mikael@sennerholm.net>
2015-06-03 18:08:19 +0200

    Add link to elk, ugly hardcoded

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,7 @@ VERSION=${GO_PIPELINE_LABEL}
 PORT_PREFIX=$1
 set -ex 
 docker rm -f doodleshop-${INSTANCE} || /bin/true
-docker run -d --name doodleshop-${INSTANCE} -p ${PORT_PREFIX}8080:8080 -p ${PORT_PREFIX}9200:9200 doodleshop:$VERSION
+# Is depending on directory name running docker-compose up in!! Ugly!!
+docker run -d --name doodleshop-${INSTANCE} --link dockerapplicationserver_elk_1:elk -p ${PORT_PREFIX}8080:8080 -p ${PORT_PREFIX}9200:9200 doodleshop:$VERSION
 
 
