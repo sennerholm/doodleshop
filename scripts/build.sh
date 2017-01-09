dd3246b8f13c67e8e38c41600efdebafe05f59f9
Mikael Sennerholm <mikael@sennerholm.net>
2017-01-09 19:39:39 +0100

    Adding build script to build in build container

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VERSION=${GO_PIPELINE_LABEL}
-docker run 
+
 docker run -it --rm --name build-doodleshop -v "$PWD":/usr/src/mymaven \
   -w /usr/src/mymaven maven:3-jdk-8 mvn versions:set \
   -DnewVersion=${env.GO_PIPELINE_LABEL} -B
