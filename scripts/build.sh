afcb15ba58f1495c2e40936778643359b7298ed3
Mikael Sennerholm <mikael@sennerholm.net>
2017-01-09 23:27:29 +0100

    Move artifacts do artifacts

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,8 +10,13 @@ set -e
 docker build --build-arg BUILD_VERSION=${VERSION} -t doodleshop-img -f Dockerfile.build .
 docker create --name doodleshop-cont doodleshop-img
 docker cp doodleshop-cont:/build/target/doodleshop.war ./doodleshop.war
-docker cp doodleshop-cont:/build/target/classes/BuildInfo.properties ./BuildInfo.properties
+#Save stuff for downstream builds in artifacts
+mkdir artifacts
+docker cp doodleshop-cont:/build/target/classes/BuildInfo.properties artifacts/BuildInfo.properties
 docker rm doodleshop-cont
 docker build --tag=${DOCKER_USER}/doodleshop:${VERSION} .
 docker login -u ${DOCKER_USER} -p $DOCKER_PASSWORD
 docker push ${DOCKER_USER}/doodleshop:${VERSION}
+
+cp -a scripts tests artifacts
+
