2f420ccd3d428c3f3bc2102f5a284ffc1e2da76f
Mikael Sennerholm <mikael@sennerholm.net>
2017-01-09 20:52:58 +0100

    No terminal on go

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,12 +3,12 @@ VERSION=${GO_PIPELINE_LABEL}
 #Assume 
 #DOCKER_USER
 #DOCKER_PASSWORD
-
+set -e
 docker pull maven:3-jdk-8
-docker run -it --rm --name build-doodleshop -v "$PWD":/usr/src/mymaven \
+docker run --rm --name build-doodleshop -v "$PWD":/usr/src/mymaven \
   -w /usr/src/mymaven maven:3-jdk-8 mvn versions:set \
   -DnewVersion=${VERSION} -B
-docker run -it --rm --name build-doodleshop -v "$PWD":/usr/src/mymaven \
+docker run --rm --name build-doodleshop -v "$PWD":/usr/src/mymaven \
   -w /usr/src/mymaven maven:3-jdk-8 mvn clean package
 
 docker build --tag=${DOCKER_USER}/doodleshop:${VERSION} .
