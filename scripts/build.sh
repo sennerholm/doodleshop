cc8e4252bb2da4e7f62f4b48f0779cad8c203721
Mikael Sennerholm <mikael@sennerholm.net>
2017-01-10 16:14:44 +0100

    Now with rancher resources in src/resources/cd and generated to target/generated-cd

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,14 +10,15 @@ set -e
 docker build --build-arg BUILD_VERSION=${VERSION}  \
   --build-arg DOCKER_USER=${DOCKER_USER} \
   -t doodleshop-img -f Dockerfile.build .
+docker rm doodleshop-cont || /bin/true
 docker create --name doodleshop-cont doodleshop-img
 docker cp doodleshop-cont:/build/target/doodleshop.war ./doodleshop.war
 #Save stuff for downstream builds in artifacts
-mkdir artifacts
+mkdir artifacts || /bin/true
 docker cp doodleshop-cont:/build/target/classes/BuildInfo.properties \
           artifacts/BuildInfo.properties
-docker cp doodleshop-cont:/build/target/classes/rancher \
-          artifacts/rancher
+docker cp doodleshop-cont:/build/target/generated-cd/rancher \
+          artifacts
 docker rm doodleshop-cont
 docker build --tag=${DOCKER_USER}/doodleshop:${VERSION} .
 docker login -u ${DOCKER_USER} -p $DOCKER_PASSWORD
