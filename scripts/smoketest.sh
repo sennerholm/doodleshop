9bb3bf502866cc9de34abc38b7fc01ec8f542bf1
Mikael Sennerholm <mikael@sennerholm.net>
2017-01-12 20:41:25 +0100

    Make doodleshop work with rancher

--- scripts/smoketest.sh
+++ scripts/smoketest.sh
@@ -3,9 +3,15 @@
 INSTANCE=$DOODLE_ENV
 PORT=8080
 
+set -e
+if [ -n "${RANCHER_URL}"]; then
+	echo "Smoke on rancher"
+	DOODLE_IP=doodlelb.${GO_PIPELINE_NAME}.rancher.internal
+else
 # Get the IPAdress of the docker instans
 DOODLE_IP=`docker inspect doodleshop-${INSTANCE} | grep IPAddress | awk -F'\"' '{print $4}'`
-# Basic smoketest, TODO: should be changed to environment?
+fi
+
 if ! `curl -f http://${DOODLE_IP}:${PORT}/doodles?author=Homer | grep 'Ummmmm' >/dev/null`
 then
     echo "Test failed (curl http://${DOODLE_IP}:${PORT}/doodles?author=Homer | grep 'Ummmmm' >/dev/null)"
